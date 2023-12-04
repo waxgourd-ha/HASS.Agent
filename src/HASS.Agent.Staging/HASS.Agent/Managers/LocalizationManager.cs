40f3992d6cedd8278a3ca1d96d395a9a7d1d6db2
Amadeo Alex <68441479+amadeo-alex@users.noreply.github.com>
2023-12-04 21:50:49 +0100

    updated documentation links

--- src/HASS.Agent.Staging/HASS.Agent/Managers/LocalizationManager.cs
+++ src/HASS.Agent.Staging/HASS.Agent/Managers/LocalizationManager.cs
@@ -47,7 +47,7 @@ namespace HASS.Agent.Managers
                 if (Variables.SupportedUILanguages.All(x => x.Name != culture.Name))
                 {
                     Log.Warning("[LOCALIZATION] The selected UI culture isn't yet supported, please help out by translating:");
-                    Log.Warning("[LOCALIZATION] https://www.hass-agent.io/latest/contributing/");
+                    Log.Warning("[LOCALIZATION] https://www.hass-agent.io/latest/contributing/translating/");
 
                     SetDefaultUILanguage();
                     return;
@@ -95,7 +95,7 @@ namespace HASS.Agent.Managers
                 if (Variables.SupportedUILanguages.All(x => x.Name != culture.Name))
                 {
                     Log.Warning("[LOCALIZATION] The new UI culture isn't yet supported, please help out by translating:");
-                    Log.Warning("[LOCALIZATION] https://www.hass-agent.io/latest/contributing/");
+                    Log.Warning("[LOCALIZATION] https://www.hass-agent.io/latest/contributing/translating/");
 
                     SetStoredUILanguage();
                     return;
