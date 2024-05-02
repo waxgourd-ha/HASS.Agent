07a76e5e99afa87004c308c14b048f7649dbcd8a
amadeo-alex <68441479+amadeo-alex@users.noreply.github.com>
2024-05-02 11:35:36 +0200

    Feature: ActiveWindowSensor process name attribute (#77)
    This PR adds "ProcessName" attribute to the ActiveWindowSensor.

--- src/HASS.Agent/HASS.Agent.Shared/HomeAssistant/Sensors/GeneralSensors/SingleValue/ActiveWindowSensor.cs
+++ src/HASS.Agent/HASS.Agent.Shared/HomeAssistant/Sensors/GeneralSensors/SingleValue/ActiveWindowSensor.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
 using HASS.Agent.Shared.Models.HomeAssistant;
+using Newtonsoft.Json;
 
 namespace HASS.Agent.Shared.HomeAssistant.Sensors.GeneralSensors.SingleValue
 {
@@ -12,7 +14,9 @@ namespace HASS.Agent.Shared.HomeAssistant.Sensors.GeneralSensors.SingleValue
     {
         private const string DefaultName = "activewindow";
 
-        public ActiveWindowSensor(int? updateInterval = null, string entityName = DefaultName, string name = DefaultName, string id = default) : base(entityName ?? DefaultName, name ?? null, updateInterval ?? 15, id) { }
+        private string _processName = string.Empty;
+
+        public ActiveWindowSensor(int? updateInterval = null, string entityName = DefaultName, string name = DefaultName, string id = default) : base(entityName ?? DefaultName, name ?? null, updateInterval ?? 15, id, true) { }
 
         public override DiscoveryConfigModel GetAutoDiscoveryConfig()
         {
@@ -31,16 +35,29 @@ namespace HASS.Agent.Shared.HomeAssistant.Sensors.GeneralSensors.SingleValue
                 Device = deviceConfig,
                 State_topic = $"{Variables.MqttManager.MqttDiscoveryPrefix()}/{Domain}/{deviceConfig.Name}/{ObjectId}/state",
                 Icon = "mdi:window-maximize",
-                Availability_topic = $"{Variables.MqttManager.MqttDiscoveryPrefix()}/{Domain}/{deviceConfig.Name}/availability"
+                Availability_topic = $"{Variables.MqttManager.MqttDiscoveryPrefix()}/{Domain}/{deviceConfig.Name}/availability",
+                Json_attributes_topic = $"{Variables.MqttManager.MqttDiscoveryPrefix()}/{Domain}/{deviceConfig.Name}/{ObjectId}/attributes"
             });
         }
 
         public override string GetState()
         {
-            return GetActiveWindowTitle();
+            var windowHandle = GetForegroundWindow();
+
+            var returnValue = GetWindowThreadProcessId(windowHandle, out var processId);
+            if (returnValue != 0 && processId != 0)
+            {
+                using var process = Process.GetProcessById(Convert.ToInt32(processId));
+                _processName = process.ProcessName ?? string.Empty;
+            }
+
+            return GetWindowTitle(windowHandle);
         }
 
-        public override string GetAttributes() => string.Empty;
+        public override string GetAttributes() => JsonConvert.SerializeObject(new
+        {
+            processName = _processName
+        });
 
         [DllImport("user32.dll")]
         private static extern IntPtr GetForegroundWindow();
@@ -51,14 +68,16 @@ namespace HASS.Agent.Shared.HomeAssistant.Sensors.GeneralSensors.SingleValue
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         private static extern int GetWindowText(IntPtr hWnd, StringBuilder builder, int count);
 
-        private static string GetActiveWindowTitle()
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
+
+        private static string GetWindowTitle(IntPtr windowHandle)
         {
-            var windowHandle = GetForegroundWindow();
             var titleLength = GetWindowTextLength(windowHandle) + 1;
             var builder = new StringBuilder(titleLength);
             var windowTitle = GetWindowText(windowHandle, builder, titleLength) > 0 ? builder.ToString() : string.Empty;
 
-            return windowTitle.Length > 255 ? windowTitle[..255]: windowTitle; //Note(Amadeo): to make sure we don't exceed HA limitation of 255 payload length
+            return windowTitle.Length > 255 ? windowTitle[..255] : windowTitle; //Note(Amadeo): to make sure we don't exceed HA limitation of 255 payload length
         }
     }
 }
