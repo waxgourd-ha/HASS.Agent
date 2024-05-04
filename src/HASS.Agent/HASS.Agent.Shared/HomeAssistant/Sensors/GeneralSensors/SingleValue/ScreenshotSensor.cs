2ff369689bc72ae8e9be3615cf0ae75498597084
amadeo-alex <68441479+amadeo-alex@users.noreply.github.com>
2024-05-04 11:48:45 +0200

    Fix: screenshot sensor monitor scaling (#84)
    This PR fixes screenshot sensor capturing only part of the screen when "non 100%" scaling.
    Thanks for @ElDingo424 for reporting - #81

--- src/HASS.Agent/HASS.Agent.Shared/HomeAssistant/Sensors/GeneralSensors/SingleValue/ScreenshotSensor.cs
+++ src/HASS.Agent/HASS.Agent.Shared/HomeAssistant/Sensors/GeneralSensors/SingleValue/ScreenshotSensor.cs
@@ -13,11 +13,17 @@ using System.Xml.Linq;
 using System.Runtime.InteropServices;
 using System.Drawing.Imaging;
 using System.Text.RegularExpressions;
+using Windows.Graphics.Display;
 
 namespace HASS.Agent.Shared.HomeAssistant.Sensors.GeneralSensors.SingleValue;
 public class ScreenshotSensor : AbstractSingleValueSensor
 {
     private const string DefaultName = "screenshot";
+
+    private DEVMODE _devMode = new()
+    {
+        dmSize = (short)Marshal.SizeOf(typeof(DEVMODE))
+    };
 
     public int ScreenIndex;
 
@@ -52,14 +58,14 @@ public class ScreenshotSensor : AbstractSingleValueSensor
 
     public override string GetState()
     {
-        var screenCount = Screen.AllScreens.Length;
-        if (ScreenIndex >= screenCount || ScreenIndex < 0)
+        if (ScreenIndex >= Screen.AllScreens.Length || ScreenIndex < 0)
         {
             Log.Warning("[SCREENSHOT] Wrong index '{index}' - returning image for screen 0", ScreenIndex);
             ScreenIndex = 0;
         }
 
         var screenImage = CaptureScreen(ScreenIndex);
+
         return Convert.ToBase64String(screenImage);
     }
 
@@ -72,22 +78,73 @@ public class ScreenshotSensor : AbstractSingleValueSensor
         catch (Exception ex)
         {
             Log.Error(ex, "[SCREENSHOT] Internal Error capturing screen {index}, {ex}", ex.Message);
+
             return Array.Empty<byte>();
         }
     }
 
     private byte[] CapturePngFile(Screen screen)
     {
+        EnumDisplaySettings(screen.DeviceName, -1, ref _devMode);
+
+        var scalingFactor = Math.Round(decimal.Divide(_devMode.dmPelsWidth, screen.Bounds.Width), 2);
+
         var captureRectangle = screen.Bounds;
-        var captureBitmap = new Bitmap(captureRectangle.Width, captureRectangle.Height, PixelFormat.Format32bppArgb);
-        var captureGraphics = Graphics.FromImage(captureBitmap);
-        captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
+        var captureSize = new Size(
+            Convert.ToInt32(screen.Bounds.Width * scalingFactor),
+            Convert.ToInt32(screen.Bounds.Height * scalingFactor)
+        );
+        using var captureBitmap = new Bitmap(captureSize.Width, captureSize.Height, PixelFormat.Format32bppArgb);
+        using var captureGraphics = Graphics.FromImage(captureBitmap);
+        captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureSize);
 
-        using var ms = new MemoryStream();
-        captureBitmap.Save(ms, ImageFormat.Png);
+        using var memoryStream = new MemoryStream();
+        captureBitmap.Save(memoryStream, ImageFormat.Png);
 
-        return ms.ToArray();
+        return memoryStream.ToArray();
     }
 
     public override string GetAttributes() => string.Empty;
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct DEVMODE
+    {
+        private const int CCHDEVICENAME = 0x20;
+        private const int CCHFORMNAME = 0x20;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+        public string dmDeviceName;
+        public short dmSpecVersion;
+        public short dmDriverVersion;
+        public short dmSize;
+        public short dmDriverExtra;
+        public int dmFields;
+        public int dmPositionX;
+        public int dmPositionY;
+        public ScreenOrientation dmDisplayOrientation;
+        public int dmDisplayFixedOutput;
+        public short dmColor;
+        public short dmDuplex;
+        public short dmYResolution;
+        public short dmTTOption;
+        public short dmCollate;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+        public string dmFormName;
+        public short dmLogPixels;
+        public int dmBitsPerPel;
+        public int dmPelsWidth;
+        public int dmPelsHeight;
+        public int dmDisplayFlags;
+        public int dmDisplayFrequency;
+        public int dmICMMethod;
+        public int dmICMIntent;
+        public int dmMediaType;
+        public int dmDitherType;
+        public int dmReserved1;
+        public int dmReserved2;
+        public int dmPanningWidth;
+        public int dmPanningHeight;
+    }
+
+    [DllImport("user32.dll")]
+    private static extern bool EnumDisplaySettings(string lpszDeviceName, int iModeNum, ref DEVMODE lpDevMode);
 }
