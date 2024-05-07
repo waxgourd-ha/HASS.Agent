efa9a323bbe5bbfc160aedf71b55f87d83d8846a
amadeo-alex <68441479+amadeo-alex@users.noreply.github.com>
2024-05-07 15:13:48 +0200

    Fix: name being used instead of friendly name while creating/editing commands (#88)
    This PR fixed commands friendly name not being properly saved when creating/editing sensors - entity name was used instead.

--- src/HASS.Agent/HASS.Agent/Forms/Commands/CommandsMod.cs
+++ src/HASS.Agent/HASS.Agent/Forms/Commands/CommandsMod.cs
@@ -265,7 +265,7 @@ namespace HASS.Agent.Forms.Commands
                 MessageBoxAdv.Show(this, Languages.CommandsMod_BtnStore_DeviceNameInSensorName, Variables.MessageBoxTitle, MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
 
-			var friendlyName = string.IsNullOrEmpty(TbName.Text.Trim()) ? name : TbName.Text.Trim();
+			var friendlyName = string.IsNullOrEmpty(TbFriendlyName.Text.Trim()) ? name : TbFriendlyName.Text.Trim();
 
 			var sanitized = SharedHelperFunctions.GetSafeValue(name);
 			if (sanitized != name)
